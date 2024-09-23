81044a4f34cbca6193bab512670b6133bd788195
mahaonan001 <1649801526@qq.com>
2024-09-23 19:59:12 +0800

    deleted:    cmd/client/main.go 	new file:   cmd/master/internal/regist/regist.go 	renamed:    cmd/node/main.go -> cmd/master/master.go 	new file:   cmd/master/model/streamPoint.go 	deleted:    cmd/server/main.go 	deleted:    proto/dsfs.pb.go 	deleted:    proto/dsfs.proto 	deleted:    proto/dsfs_grpc.pb.go

---
+++ cmd/master/model/streamPoint.go
@@ -0,0 +1,33 @@
+package nodeModel
+
+import "sync"
+
+type NodeServer int
+
+type NodeBelong struct {
+	Node NodeServer
+	Path string
+}
+
+type StreamPoint struct {
+	sync.Mutex
+
+	FileName  string
+	SpiltNode []NodeBelong
+	numNode   int
+}
+
+var CountNumber NodeServer = -1
+
+func (s *StreamPoint) GetNumbersofNode() int {
+	return s.numNode
+}
+
+func (s *StreamPoint) GetNodeNumber() NodeServer {
+	s.Lock()
+	defer s.Unlock()
+	s.numNode++
+	CountNumber++
+	s.SpiltNode = append(s.SpiltNode, NodeBelong{CountNumber, s.FileName})
+	return CountNumber
+}
