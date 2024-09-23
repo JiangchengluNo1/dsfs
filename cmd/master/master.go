d2fccf829711994113cec67bde20c018e255e2f8
mahaonan001 <1649801526@qq.com>
2024-09-23 22:49:05 +0800

    modified:   cmd/master/master.go 	new file:   proto/filetransfer.pb.go 	new file:   proto/filetransfer.proto 	new file:   proto/filetransfer_grpc.pb.go 	deleted:    test/main_test.go

--- cmd/master/master.go
+++ cmd/master/master.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"io"
+
+	filetransfer "github.com/mahaonan001/dsfs/proto"
+)
+
+type FileServer struct {
+	filetransfer.UnimplementedFileServiceServer
+}
+
+// 实现文件上传
+func (f *FileServer) UploadFile(stream filetransfer.FileService_UploadServer) error {
+	for {
+		fileChunk, err := stream.Recv()
+		if err == io.EOF {
+
+		} else if err != nil {
+			return err
+		}
+	}
+}
+
 func main() {
 
 }
