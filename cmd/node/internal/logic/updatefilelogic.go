73df5d43d87983981665ab19ad5deebdbbec55f2
mahaonan001 <1649801526@qq.com>
2024-11-07 14:09:12 +0800

    init the first idea of upload

--- cmd/node/internal/logic/updatefilelogic.go
+++ cmd/node/internal/logic/updatefilelogic.go
@@ -1,5 +1,15 @@
 package logic
 
 func UpdateFile(filemeta string, up_data []byte) error {
+	filemeta += ".up"
+	file, err := OpenOrCreateFile(filemeta)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = file.Write(up_data)
+	if err != nil {
+		return err
+	}
 	return nil
 }
