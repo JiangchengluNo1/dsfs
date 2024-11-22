e1f7164aa0d00ca5f310fa0623cbf99e281a1e62
mahaonan001 <1649801526@qq.com>
2024-11-22 09:19:48 +0800

    add the master ClientOffline Sounder function

--- cmd/master/internal/master/master.go
+++ cmd/master/internal/master/master.go
@@ -2,12 +2,18 @@ package master
 
 import (
 	"context"
+	"fmt"
+	"sync"
+	"time"
 
 	noding "github.com/mahaonan001/dsfs/proto/healthing"
 )
 
 type MasterServer struct {
 	noding.UnimplementedNodingServer
+	NodeClient map[int32]time.Time // client onilne or not
+	FileOnNode map[string]int32    // file to client
+	sync.RWMutex
 }
 
 // //////////////////////////////////////
@@ -16,7 +22,8 @@ var file2number map[string][]int32
 // //////////////////////////////////////
 func (m *MasterServer) HealthCheck(ctx context.Context, in *noding.Hearting) (*noding.HeartingResponse, error) {
 	beatnumber := in.GetBeatNumber()
-	return &noding.HeartingResponse{Success: CheckNodeOnline(beatnumber)}, nil
+	m.NodeClient[beatnumber] = time.Now()
+	return &noding.HeartingResponse{Success: m.CheckNodeOnline(beatnumber)}, nil
 }
 
 func (m *MasterServer) WakeUp(ctx context.Context, in *noding.WakeUp) (*noding.WakeUpResponse, error) {
@@ -27,6 +34,21 @@ func (m *MasterServer) WakeUp(ctx context.Context, in *noding.WakeUp) (*noding.W
 	}
 	return &noding.WakeUpResponse{Success: true}, nil
 }
-func CheckNodeOnline(beatnumber int32) bool {
-	return true
+func (m *MasterServer) CheckNodeOnline(beatnumber int32) bool {
+	m.RLock()
+	defer m.RUnlock()
+	_, ok := m.NodeClient[beatnumber]
+	return ok
+}
+
+func (m *MasterServer) ClientOffSound() {
+	m.RLock()
+	defer m.RUnlock()
+	for k := range m.NodeClient {
+		if time.Since(m.NodeClient[k]) > time.Second*10 {
+			red := "\033[31m"
+			reset := "\033[0m"
+			fmt.Printf("%sNode %v is out of line\n%s", red, k, reset)
+		}
+	}
 }
